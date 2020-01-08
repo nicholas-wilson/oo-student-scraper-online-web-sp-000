622d2dcbbe71c772b1f8aa909d09cd09bf9e0983
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-08 22:42:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,11 +21,29 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     profile_page = Nokogiri::HTML(open(profile_url))
     social_container = profile_page.css(".social-icon-container a")
+    profile = {}
+    if social_container.size == 1
+      profile[:twitter] = social_container.attribute("href").value
+    elsif social_container.size == 2
+      profile[:twitter] = social_container[0].attribute("href").value
+      profile[:linkedin] = social_container[1].attribute("href").value
+    elsif social_container.size == 3
+      profile[:twitter] = social_container[0].attribute("href").value
+      profile[:linkedin] = social_container[1].attribute("href").value
+      profile[:github] = social_container[2].attribute("href").value
+    elsif social_container.size == 4
+      profile[:twitter] = social_container[0].attribute("href").value
+      profile[:linkedin] = social_container[1].attribute("href").value
+      profile[:github] = social_container[2].attribute("href").value
+      profile[:blog] = social_container[3].attribute("href").value
+    end
+    profile[:profile_quote] = profile_page.css(".profile-quote").text
+    profile[:bio] = profile_page.css(".bio-content.content-holder p").text
     {
-      :twitter => social_container[0].attribute("href").value || "",
-      :linkedin => social_container[1].attribute("href").value || "",
-      :github => social_container[2].attribute("href").value || "",
-      :blog => social_container[3].attribute("href").value || "",
+      :twitter => social_container[0].attribute("href").value,
+      :linkedin => social_container[1].attribute("href").value,
+      :github => social_container[2].attribute("href").value,
+      :blog => social_container[3].attribute("href").value,
       :profile_quote => profile_page.css(".profile-quote").text,
       :bio => profile_page.css(".bio-content.content-holder p").text
     }
