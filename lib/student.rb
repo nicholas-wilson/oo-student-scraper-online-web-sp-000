52c8a45cb26832d5d09b7025cb9e283cc42df259
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-08 23:22:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,8 +32,7 @@ class Student
   def self.create_from_collection(students_array)
     binding.pry
     students_array.each do |student|
-      info = Scraper.scrape_index_page(student)
-      Student.new(info)
+      Student.new(student)
     end
   end
 
